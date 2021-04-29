47b9967b663dc728b97c2617d02a9d6bf3a91e9d
Robert Brunson <siinspam@gmail.com>
2021-04-28 23:45:16 -0400

    creative v0

---
+++ src/main/java/tech/quilldev/stratumcreative/StratumCreative.java
@@ -0,0 +1,17 @@
+package tech.quilldev.stratumcreative;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StratumCreative extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
